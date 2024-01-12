292532444911fd8a3a9a1c199e22d6e118d9ef7a
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-12 13:11:32 -0800

    Use ray actor as lock
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- health.py
+++ health.py
@@ -1,18 +1,31 @@
-import os
 import logging
 from typing import Dict
-from filelock import FileLock
 
 from starlette.requests import Request
 
+import ray
 from ray import serve
+from ray.serve._private.constants import SERVE_NAMESPACE
 from ray.serve.handle import DeploymentHandle
 
 
 logger = logging.getLogger("ray.serve")
 
 
-HEALTH_FILE = "/tmp/health.txt"
+HEALTH_LOCK_ACTOR_NAME = "health_lock"
+
+
+@ray.remote(name=HEALTH_LOCK_ACTOR_NAME, namespace=SERVE_NAMESPACE, lifetime="detached")
+class HealthLock:
+
+    def __init__(self):
+        self._healthy = True
+    
+    def set_health(self, healthy: bool):
+        self._healthy = healthy
+    
+    def is_healthy(self) -> bool:
+        return self._healthy
 
 
 @serve.deployment(ray_actor_options={"num_cpus": 0})
@@ -21,31 +34,23 @@ class Writer:
 
     def __init__(self, health_deployment: DeploymentHandle):
         self.health_deployment = health_deployment
-        if self._read_health() not in ["healthy", "unhealthy"]:
-            self._write_health("healthy")
+        try:
+            self.health_lock_actor = HealthLock.remote()
+        except ValueError:
+            self.health_lock_actor = ray.get_actor(
+                name=HEALTH_LOCK_ACTOR_NAME, namespace=SERVE_NAMESPACE
+            )
 
     async def __call__(self, request: Request):
         request_json: Dict = await request.json()
         logger.info(f"Received request: {request_json}")
-        self._write_health(request_json["write"])
-        logger.info(f"Wrote {request_json['write']} to health file.")
+        if request_json.get("write") == "healthy":
+            logger.info(f"Making replicas healthy.")
+            await self.health_lock_actor.set_health.remote(True)
+        elif request_json.get("write") == "unhealthy":
+            logger.info(f"Making replicas unhealthy.")
+            await self.health_lock_actor.set_health.remote(False)
         return "Hello!"
-    
-    def _read_health(self) -> str:
-        with FileLock(f"{HEALTH_FILE}.lock"):
-            if not os.path.exists(HEALTH_FILE):
-                f = open(HEALTH_FILE, "w+")
-                f.close()
-                return
-            else:
-                with open(HEALTH_FILE, "r+") as f:
-                    return f.read()
-    
-    def _write_health(self, content: str):
-        with FileLock(f"{HEALTH_FILE}.lock"):
-            with open(HEALTH_FILE, "w+") as f:
-                f.write(content)
-                f.flush()
 
 
 @serve.deployment(ray_actor_options={"num_cpus": 0})
@@ -56,19 +61,17 @@ class Health:
     node, and on the same node as the writer.
     """
 
-    def check_health(self):
-        if not self._read_health() == "healthy":
+    def __init__(self):
+        try:
+            self.health_lock_actor = HealthLock.remote()
+        except ValueError:
+            self.health_lock_actor = ray.get_actor(
+                name=HEALTH_LOCK_ACTOR_NAME, namespace=SERVE_NAMESPACE
+            )
+
+    async def check_health(self):
+        if not (await self.health_lock_actor.is_healthy.remote()):
             raise RuntimeError("Not healthy!")
-    
-    def _read_health(self) -> str:
-        with FileLock(f"{HEALTH_FILE}.lock"):
-            if not os.path.exists(HEALTH_FILE):
-                f = open(HEALTH_FILE, "w+")
-                f.close()
-                return
-            else:
-                with open(HEALTH_FILE, "r+") as f:
-                    return f.read()
 
 
 app = Writer.bind(Health.bind())
